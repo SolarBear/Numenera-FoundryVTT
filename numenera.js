ed9deef71c21c009f8eb8ca786728a6635510cf3
SolarBear <7608394+SolarBear@users.noreply.github.com>
2020-04-30 22:24:33 -0400

    Initiative tracker now fully functional

--- numenera.js
+++ numenera.js
@@ -1,8 +1,12 @@
 import { NumeneraActor } from './module/actor/NumeneraActor.js';
 import { NumeneraNPCActorSheet } from './module/actor/sheets/NumeneraNPCActorSheet.js';
 import { NumeneraPCActorSheet } from './module/actor/sheets/NumeneraPCActorSheet.js';
+
 import { NUMENERA } from './module/config.js';
+import { getInitiativeFormula, rollInitiative } from './module/combat.js';
+import { rollText } from './module/roll.js';
 import { preloadHandlebarsTemplates } from './module/templates.js';
+
 import { NumeneraItem } from './module/item/NumeneraItem.js';
 import { NumeneraArmorItemSheet } from './module/item/sheets/NumeneraArmorItemSheet.js';
 import { NumeneraArtifactItemSheet } from './module/item/sheets/NumeneraArtifactItemSheet.js';
@@ -10,6 +14,8 @@ import { NumeneraCypherItemSheet } from './module/item/sheets/NumeneraCypherItem
 import { NumeneraEquipmentItemSheet } from './module/item/sheets/NumeneraEquipmentItemSheet.js';
 import { NumeneraOddityItemSheet } from './module/item/sheets/NumeneraOddityItemSheet.js';
 import { NumeneraWeaponItemSheet } from './module/item/sheets/NumeneraWeaponItemSheet.js';
+
+import { NumeneraChatMessage } from './module/chat.js';
 
 Hooks.once("init", function() {
     console.log('Numenera | Initializing Numenera System');
@@ -22,7 +28,8 @@ Hooks.once("init", function() {
     CONFIG.Item.entityClass = NumeneraItem;
 
     //Each type of Actor will provide its own personal, free-range, bio, nut-free formula.
-    Combat.prototype._getInitiativeFormula = combatant => combatant.actor.getInitiativeFormula();
+    Combat.prototype._getInitiativeFormula = getInitiativeFormula;
+    Combat.prototype.rollInitiative = rollInitiative;
 
     // Register sheet application classes
     Actors.unregisterSheet("core", ActorSheet);
@@ -37,20 +44,39 @@ Hooks.once("init", function() {
     Items.registerSheet("numenera", NumeneraOddityItemSheet, { types: ["oddity"], makeDefault: true });
     Items.registerSheet("numenera", NumeneraWeaponItemSheet, { types: ["weapon"], makeDefault: true });
 
-    // Preload Handlebars Templates
     preloadHandlebarsTemplates();
 });
   
-  /*
-  Display an NPC's difficulty between parentheses in the Actors list
-  */
-  Hooks.on('renderActorDirectory', (app, html, options) => {
-    const found = html.find(".entity-name");
-    
-    app.entities
-        .filter(actor => actor.data.type === 'npc')
-        .forEach(actor => {
-            found.filter((i, elem) => elem.innerText === actor.data.name)
-                 .each((i, elem) => elem.innerText += ` (${actor.data.data.level * 3})`);
-        })
+/*
+Display an NPC's difficulty between parentheses in the Actors list
+*/
+Hooks.on('renderActorDirectory', (app, html, options) => {
+  const found = html.find(".entity-name");
+  
+  app.entities
+      .filter(actor => actor.data.type === 'npc')
+      .forEach(actor => {
+          found.filter((i, elem) => elem.innerText === actor.data.name)
+                .each((i, elem) => elem.innerText += ` (${actor.data.data.level * 3})`);
+      })
 });
+
+Hooks.on("renderChatMessage", (app, html, data) => {
+    //Here, "app" is the ChatMessage object
+    if (app.roll.dice[0].faces === 20)
+    {
+        const dieRoll = app.roll.dice[0].rolls[0].roll;
+        const special = rollText(dieRoll);
+
+        if (!special)
+            return;
+
+        const { text, color } = special;
+
+        const newContent = `<span class="numenera-message-special">${text}</span>`;
+
+        const dt = html.find("h4.dice-total");
+        $(newContent).insertBefore(dt);
+    }
+});
+  
