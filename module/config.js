997d38e679cd22dc7767d1e1bc4a3fc6535f4094
SolarBear <7608394+SolarBear@users.noreply.github.com>
2020-05-26 22:38:16 -0400

    Added languages to system, first few strings

--- module/config.js
+++ module/config.js
@@ -1,114 +1,114 @@
 export const NUMENERA = {};
 
 NUMENERA.weightClasses = [
-  'Light',
-  'Medium',
-  'Heavy'
+  "NUMENERA.weightClasses.Light",
+  "NUMENERA.weightClasses.Medium",
+  "NUMENERA.weightClasses.Heavy",
 ];
 
-NUMENERA.optionalWeightClasses = ["N/A"].concat(NUMENERA.weightClasses);
+NUMENERA.optionalWeightClasses = ["NUMENERA.N/A"].concat(NUMENERA.weightClasses);
 
 NUMENERA.weaponTypes = [
-  'Bashing',
-  'Bladed',
-  'Ranged',
+  "NUMENERA.Bashing",
+  "NUMENERA.Bladed",
+  "NUMENERA.Ranged",
 ];
 
 NUMENERA.stats = [
-  'Might',
-  'Speed',
-  'Intellect',
+  "Might",
+  "Speed",
+  "Intellect",
 ];
 
 NUMENERA.skillLevels = {
-  'i': 'Inability',
-  'u': 'Untrained',
-  't': 'Trained',
-  's': 'Specialized'
+  "i": "Inability",
+  "u": "Untrained",
+  "t": "Trained",
+  "s": "Specialized"
 };
 
 NUMENERA.types = [
   {
-    abbrev: 'a',
-    name: 'Arkus',
+    abbrev: "a",
+    name: "Arkus",
   },
   {
-    abbrev: 'd',
-    name: 'Delve',
+    abbrev: "d",
+    name: "Delve",
   },
   {
-    abbrev: 'g',
-    name: 'Glaive',
+    abbrev: "g",
+    name: "Glaive",
   },
   {
-    abbrev: 'j',
-    name: 'Jack',
+    abbrev: "j",
+    name: "Jack",
   },
   {
-    abbrev: 'n',
-    name: 'Nano',
+    abbrev: "n",
+    name: "Nano",
   },
   {
-    abbrev: 'w',
-    name: 'Wright',
+    abbrev: "w",
+    name: "Wright",
   },
 ];
 
 NUMENERA.typePowers = {
-  'g': 'Combat Maneuvers',
-  'j': 'Tricks of the Trade',
-  'n': 'Esoteries',
-  'a': 'Precepts',
-  'd': 'Delve Lores',
-  'w': 'Inspired Techniques',
+  "g": "Combat Maneuvers",
+  "j": "Tricks of the Trade",
+  "n": "Esoteries",
+  "a": "Precepts",
+  "d": "Delve Lores",
+  "w": "Inspired Techniques",
 };
 
 NUMENERA.damageTrack = [
   {
-    label: 'Hale',
-    description: 'Normal state for a character.'
+    label: "Hale",
+    description: "Normal state for a character."
   },
   {
-    label: 'Impaired',
-    description: 'In a wounded or injured state. Applying Effort costs 1 extra point per effort level applied.'
+    label: "Impaired",
+    description: "In a wounded or injured state. Applying Effort costs 1 extra point per effort level applied."
   },
   {
-    label: 'Debilitated',
-    description: 'In a critically injured state. The character can do no other action than to crawl an immediate distance; if their Speed pool is 0, they cannot move at all.'
+    label: "Debilitated",
+    description: "In a critically injured state. The character can do no other action than to crawl an immediate distance; if their Speed pool is 0, they cannot move at all."
   },
   {
-    label: 'Dead',
-    description: 'The character is dead.'
+    label: "Dead",
+    description: "The character is dead."
   }
 ];
 
 NUMENERA.recoveries = {
-  'action': '1 Action',
-  'tenMin': '10 min',
-  'oneHour': '1 hour',
-  'tenHours': '10 hours'
+  "action": "1 Action",
+  "tenMin": "10 min",
+  "oneHour": "1 hour",
+  "tenHours": "10 hours"
 };
 
 NUMENERA.advances = {
-  'statPools': '+4 to stat pools',
-  'effort': '+1 to Effort',
-  'edge': '+1 to Edge',
-  'skillTraining': 'Train/specialize skill',
-  'other': 'Other',
+  "statPools": "+4 to stat pools",
+  "effort": "+1 to Effort",
+  "edge": "+1 to Edge",
+  "skillTraining": "Train/specialize skill",
+  "other": "Other",
 };
 
 NUMENERA.ranges = [
-  'Immediate',
-  'Short',
-  'Long',
-  'Very Long'
+  "Immediate",
+  "Short",
+  "Long",
+  "Very Long"
 ];
 
 NUMENERA.optionalRanges = ["N/A"].concat(NUMENERA.ranges);
 
 NUMENERA.abilityTypes = [
-  'Action',
-  'Enabler',
+  "Action",
+  "Enabler",
 ];
 
 NUMENERA.cypherTypes = [
