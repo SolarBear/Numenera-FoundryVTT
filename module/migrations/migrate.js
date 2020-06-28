cc60d27f4e53ea23ddab90a98640e903b63e7331
SolarBear <7608394+SolarBear@users.noreply.github.com>
2020-06-28 13:51:12 -0400

    Bug fix, put item version filter at same place

--- module/migrations/migrate.js
+++ module/migrations/migrate.js
@@ -15,14 +15,17 @@ export async function migrateWorld() {
   let npcActors = game.actors.entities.filter(actor => actor.data.type === 'npc' && actor.data.data.version < currentNPCActorVersion);
 
   //Items - compendium/item directory Items are kept in a different location than Actor-owned Items!
-  const nonActorItems = game.items.entities.filter(item => item.data.data.version < currentItemVersion);
-  const actorItems = game.actors.entities.map(a => a.items).filter(m => m.size > 0).flatMap(m => Array.from(m.values()));
-  const items = [...nonActorItems, ...actorItems];
+  const nonActorItems = game.items.entities;
+  const actorItems = game.actors.entities
+                      .map(a => a.items)
+                      .filter(m => m.size > 0)
+                      .flatMap(m => Array.from(m.values()));
+  const items = [...nonActorItems, ...actorItems].filter(item => item.data.data.version < currentItemVersion);
 
   if (pcActors && pcActors.length > 0 || npcActors && npcActors.length > 0 || items && items.length > 0) {
     ui.notifications.info(`Applying Numenera system migrations. Please be patient and do not close your game or shut down your server.`, {permanent: true});
 
-    async function migrateCollection(migrator, collection) {
+    async function migrateCollection(migrator, collection, name) {
       try {
         if (collection && collection.length > 0) {
           const updatedData = await Promise.all(collection.map(async obj => await migrator.migrate(obj)));
@@ -33,16 +36,16 @@ export async function migrateWorld() {
             }
           }
           
-          console.log(`${migrator.forType} migration succeeded!`);
+          console.log(`${name} migration succeeded!`);
         }
       } catch (e) {
-        console.error(`Error ${migrator.forType}  migrations`, e);
+        console.error(`Error in ${name} migrations`, e);
       }
     }
 
-    migrateCollection(PCActorMigrator, pcActors);
-    migrateCollection(NPCActorMigrator, npcActors);
-    migrateCollection(ItemMigrator, items);
+    migrateCollection(PCActorMigrator, pcActors, "PC");
+    migrateCollection(NPCActorMigrator, npcActors, "NPC");
+    migrateCollection(ItemMigrator, items, "Item");
 
     ui.notifications.info(`Numenera system migration completed!`, {permanent: true});
   }
