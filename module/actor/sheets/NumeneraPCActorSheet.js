09f4ec17c7cb93cb7c8b9a45b5aca09ddcdd6c4f
SolarBear <7608394+SolarBear@users.noreply.github.com>
2020-07-03 15:09:06 -0400

    Ability costs are now subtracted from the related pool

--- module/actor/sheets/NumeneraPCActorSheet.js
+++ module/actor/sheets/NumeneraPCActorSheet.js
@@ -532,21 +532,20 @@ export class NumeneraPCActorSheet extends ActorSheet {
     return this.actor.rollSkill(skill, event.shiftKey);
   }
 
-  onAbilityUse(event) {
+  /**
+   * Triggered whenever the use click the "Roll" button on an Ability.
+   *
+   * @param {Event} event
+   * @memberof NumeneraPCActorSheet
+   */
+  async onAbilityUse(event) {
     event.preventDefault();
     const abilityId = event.target.closest(".ability").dataset.itemId;
 
     if (!abilityId)
       return;
 
-    //Get related skill
-    const skill = this.actor.data.items.find(i => i.data.relatedAbilityId === abilityId);
-    if (!skill) {
-      ui.notifications.warn(game.i18n.localize("NUMENERA.warnings.noSkillRelatedToAbility"));
-      return;
-    }
-
-    return this.actor.rollSkill(skill, event.shiftKey);
+    await this.actor.useAbilityById(abilityId);
   }
 
   onArtifactDepletionRoll(event) {
