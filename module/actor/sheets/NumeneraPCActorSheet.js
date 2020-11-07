bd560d0b49fd911d2a2d4c151b5ba849d1a64103
SolarBear <7608394+SolarBear@users.noreply.github.com>
2020-11-06 20:58:37 -0500

    YUGE refactor of getData() to make it manageable

--- module/actor/sheets/NumeneraPCActorSheet.js
+++ module/actor/sheets/NumeneraPCActorSheet.js
@@ -223,83 +223,77 @@ export class NumeneraPCActorSheet extends ActorSheet {
    * to insert new values or mess with existing ones.
    */
   getData() {
-    //TODO split up this whole method, it's getting messy
-
     const sheetData = super.getData();
 
-    //TODO improve this, c'mon man, you can do better!
-    let cypherTypeFlavor;
-    switch (game.settings.get("numenera", "cypherTypesFlavor")) {
-      case 1: //none
-        cypherTypeFlavor = null;
-        break;
+    this._setLabelsData(sheetData);
+    this._setCypherTypeData(sheetData);
+    this._setIconSettingsData(sheetData);
+    this._setComputedValuesData(sheetData);
+    this._setItemsData(sheetData);
 
-      case 2: //anoetic/occultic
-      cypherTypeFlavor = "numenerav1";
-        break;
+    return sheetData;
+  }
 
-      case 3: //subtle/manifest/fantastic
-        cypherTypeFlavor = "cypherSystem";
-        break;
-    }
+  /**
+   * Processes Item-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setItemsData(sheetData) {
+    sheetData.data.items = sheetData.actor.items || {};
 
-    const useCypherTypes = !!cypherTypeFlavor;
-    sheetData.displayCypherType = useCypherTypes;
-    if (useCypherTypes)
-      sheetData.cypherTypes = NUMENERA.cypherTypes[cypherTypeFlavor];
+    const itemClassMap = {
+      abilities: NumeneraAbilityItem.type,
+      armor: NumeneraArmorItem.type,
+      artifacts: NumeneraArtifactItem.type,
+      cyphers: NumeneraCypherItem.type,
+      equipment: NumeneraEquipmentItem.type,
+      oddities: NumeneraOddityItem.type,
+      powerShifts: NumeneraPowerShiftItem.type,
+      recursion: StrangeRecursionItem.type,
+      skills: NumeneraSkillItem.type,
+      weapons: NumeneraWeaponItem.type,
+    };
 
-    // SETTINGS AND FEATURES
-    sheetData.featuresUsed = [];
-    sheetData.featureSectionNames = [];
+    Object.entries(itemClassMap).forEach(([val, type]) => {
+      if (!sheetData.data.items[val])
+        sheetData.data.items[val] = sheetData.data.items.filter(i => i.type === type).sort(sortFunction)
+    });
 
-    //Is it The Strange?
-    if (game.settings.get("numenera", "useRecursions")) {
-      sheetData.isTheStrange = true;
-      sheetData.featuresUsed.push({
-        key: "recursions",
-        label: NUMENERA.tabbedFeatures.recursions,
-      });
-      sheetData.featureSectionNames.push("NUMENERA.pcActorSheet.tab.recursion");
-    }
+    this._setCyphersData(sheetData, sheetData.displayCypherType);
+    this._setFeaturesData(sheetData);
+    this._setEquipmentData(sheetData);
+    this._setAbilitiesData(sheetData);
+    this._setSkillsData(sheetData);
+  }
 
-    if (game.settings.get("numenera", "usePowerShifts")) {
-      sheetData.usePowerShifts = true;
-      sheetData.featuresUsed.push({
-        key: "powerShifts",
-        label: NUMENERA.tabbedFeatures.powerShifts,
-      });
-      sheetData.featureSectionNames.push("NUMENERA.pcActorSheet.features.powerShifts.title");
-      sheetData.powerShiftEffects = NUMENERA.powerShiftEffects;
-    }
+  /**
+   * Processes label-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setLabelsData(sheetData) {
+    sheetData.ranges = NUMENERA.ranges
+    sheetData.weaponTypes = NUMENERA.weaponTypes;
+    sheetData.weights = NUMENERA.weightClasses;
+    sheetData.optionalWeights = NUMENERA.optionalWeightClasses;
+  }
 
-    //TODO clean this up, can probably remove a value or two
-    sheetData.showFeaturesTab = sheetData.featuresUsed.length > 0;
-    sheetData.showMultipleFeatures = sheetData.featuresUsed.length > 1;
-
-    if (sheetData.featuresUsed.length === 1) {
-      sheetData.featuresTabName = game.i18n.localize(sheetData.featureSectionNames[0]);
-      sheetData.selectedFeature = sheetData.featuresUsed[0].key;
-    }
-    else if (sheetData.featuresUsed.length > 1) {
-      sheetData.featuresTabName = game.i18n.localize("NUMENERA.pcActorSheet.tab.features");
-      sheetData.selectedFeature = sheetData.featuresUsed[0].key;
-    }
-
-    if (this.selectedFeature) {
-      sheetData.selectedFeature = this.selectedFeature;
-    }
-
-    sheetData.useOddities = game.settings.get("numenera", "useOddities");
-    
-    // Add relevant data from system settings
+  /**
+   * Processes icon settings-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setIconSettingsData(sheetData) {
     sheetData.settings = {
       icons: {}
     };
-
-    //Make sure to use getFocus(), not .focus since there is some important business logic bound to it
-    sheetData.data.currentFocus = this.actor.getFocus();
-
-    sheetData.settings.currency = game.settings.get("numenera", "currency");
 
     //Icon display settings
     sheetData.settings.icons.abilities = game.settings.get("numenera", "showAbilityIcons");
@@ -307,12 +301,35 @@ export class NumeneraPCActorSheet extends ActorSheet {
     sheetData.settings.icons.numenera = game.settings.get("numenera", "showCypherIcons");
     sheetData.settings.icons.powerShifts = game.settings.get("numenera", "showPowerShiftIcons");
     sheetData.settings.icons.skills = game.settings.get("numenera", "showSkillIcons");
+  }
 
-    //Copy labels to be used as is
-    sheetData.ranges = NUMENERA.ranges
-    sheetData.weaponTypes = NUMENERA.weaponTypes;
-    sheetData.weights = NUMENERA.weightClasses;
-    sheetData.optionalWeights = NUMENERA.optionalWeightClasses;
+  /**
+   * Processes cypher type-related data for _setItemsData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setCypherTypeData(sheetData) {
+    const flavor = NumeneraCypherItem.cypherTypeFlavor;
+    
+    sheetData.displayCypherType = !!flavor;
+    if (sheetData.displayCypherType)
+      sheetData.cypherTypes = NUMENERA.cypherTypes[flavor];
+  }
+
+  /**
+   * Processes cypher type-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setComputedValuesData(sheetData) {
+    //Make sure to use getFocus(), not .focus since there is some important business logic bound to it
+    sheetData.data.currentFocus = this.actor.getFocus();
+
+    sheetData.settings.currency = game.settings.get("numenera", "currency");
 
     sheetData.stats = {};
     for (const prop in NUMENERA.stats) {
@@ -363,45 +380,92 @@ export class NumeneraPCActorSheet extends ActorSheet {
         };
       }
     );
+  }
 
-    sheetData.data.items = sheetData.actor.items || {};
+  /**
+   * Processes feature-related data (eg. recursions, power shifts, etc.) for getData().
+   * If you're calling this from another function, please consider reviewing your life's
+   * priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setFeaturesData(sheetData) {
+    sheetData.featuresUsed = [];
+    sheetData.featureSectionNames = [];
 
-    const items = sheetData.data.items;
+    //RECURSIONS
+    if (game.settings.get("numenera", "useRecursions")) {
+      sheetData.isTheStrange = true;
+      sheetData.featuresUsed.push({
+        key: "recursions",
+        label: NUMENERA.tabbedFeatures.recursions,
+      });
+      sheetData.featureSectionNames.push("NUMENERA.pcActorSheet.tab.recursion");
+    }
 
-    const itemClassMap = {
-      abilities: NumeneraAbilityItem.type,
-      armor: NumeneraArmorItem.type,
-      artifacts: NumeneraArtifactItem.type,
-      cyphers: NumeneraCypherItem.type,
-      equipment: NumeneraEquipmentItem.type,
-      skills: NumeneraSkillItem.type,
-      weapons: NumeneraWeaponItem.type,
-    };
+    //ODDITIES
+    sheetData.useOddities = game.settings.get("numenera", "useOddities");
+    if (sheetData.useOddities) {
+      sheetData.data.items.oddities = sheetData.data.items.oddities.map(oddity => {
+        oddity.editable = game.user.hasRole(game.settings.get("numenera", "cypherArtifactEdition"));
+        oddity.showIcon = oddity.img && sheetData.settings.icons.numenera;
+        oddity.data.notes = removeHtmlTags(oddity.data.notes);
+        return oddity;
+      });
+    }
 
-    if (sheetData.isTheStrange)
-      itemClassMap.recursion = StrangeRecursionItem.type;
+    //POWER SHIFTS
+    if (game.settings.get("numenera", "usePowerShifts")) {
+      sheetData.usePowerShifts = true;
+      sheetData.featuresUsed.push({
+        key: "powerShifts",
+        label: NUMENERA.tabbedFeatures.powerShifts,
+      });
+      sheetData.featureSectionNames.push("NUMENERA.pcActorSheet.features.powerShifts.title");
+      sheetData.powerShiftEffects = NUMENERA.powerShiftEffects;
+    }
 
-    if (sheetData.useOddities)
-      itemClassMap.oddities = NumeneraOddityItem.type;
-      
-    if (sheetData.usePowerShifts)
-      itemClassMap.powerShifts = NumeneraPowerShiftItem.type;
+    if (sheetData.usePowerShifts) {
+      sheetData.data.items.powerShifts = sheetData.data.items.powerShifts.map(powerShift => {
+        powerShift.showIcon = powerShift.img && sheetData.settings.icons.powerShifts;
+        powerShift.data.notes = removeHtmlTags(powerShift.data.notes);
+        return powerShift;
+      });
+    }
 
-    Object.entries(itemClassMap).forEach(([val, type]) => {
-      if (!sheetData.data.items[val])
-        sheetData.data.items[val] = items.filter(i => i.type === type).sort(sortFunction)
-    });
+    //This section MUST be the last one in _setFeaturesData
+    sheetData.showFeaturesTab = sheetData.featuresUsed.length > 0;
+    sheetData.showMultipleFeatures = sheetData.featuresUsed.length > 1;
 
-    //Make it so that unidentified artifacts and cyphers appear as blank items
-    //TODO extract this in the Item class if possible (perhaps as a static method?)
+    if (sheetData.featuresUsed.length === 1) {
+      sheetData.featuresTabName = game.i18n.localize(sheetData.featureSectionNames[0]);
+      sheetData.selectedFeature = sheetData.featuresUsed[0].key;
+    }
+    else if (sheetData.featuresUsed.length > 1) {
+      sheetData.featuresTabName = game.i18n.localize("NUMENERA.pcActorSheet.tab.features");
+      sheetData.selectedFeature = sheetData.featuresUsed[0].key;
+    }
+
+    if (this.selectedFeature) {
+      sheetData.selectedFeature = this.selectedFeature;
+    }
+  }
+
+  /**
+   * Processes cypher-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setCyphersData(sheetData, useCypherType) {
     sheetData.data.items.artifacts = sheetData.data.items.artifacts.map(artifact => {
       artifact.editable = game.user.hasRole(game.settings.get("numenera", "cypherArtifactEdition"));
 
       if (!artifact.data.identified && !artifact.editable) {
-        artifact.name = game.i18n.localize("NUMENERA.pc.numenera.artifact.unidentified");
-        artifact.data.level = game.i18n.localize("NUMENERA.unknown");
-        artifact.data.effect = game.i18n.localize("NUMENERA.unknown");
-        artifact.data.depletion = null;
+        //Make it so that unidentified artifacts appear as blank items
+        artifact.asUnidentified();
       }
       else {
         artifact.data.effect = removeHtmlTags(artifact.data.effect);
@@ -415,19 +479,14 @@ export class NumeneraPCActorSheet extends ActorSheet {
       cypher.editable = game.user.hasRole(game.settings.get("numenera", "cypherArtifactEdition"));
 
       if (!cypher.data.identified && !cypher.editable) {
-        cypher.name = game.i18n.localize("NUMENERA.pc.numenera.cypher.unidentified");
-        cypher.data.level = game.i18n.localize("NUMENERA.unknown");
-        cypher.data.effect = game.i18n.localize("NUMENERA.unknown");
-
-        if (useCypherTypes) {
-          cypher.data.cypherType = game.i18n.localize("NUMENERA.unknown");
-        }
+        //Make it so that unidentified cyphers appear as blank items
+        cypher.asUnidentified();
       }
       else {
         cypher.data.effect = removeHtmlTags(cypher.data.effect);
       }
 
-      if (useCypherTypes && cypher.data.identified && !cypher.data.cypherType) {
+      if (useCypherType && cypher.data.identified && !cypher.data.cypherType) {
         //Use the very first object key as property since none has been defined yet
         cypher.data.cypherType = Object.keys(NUMENERA.cypherTypes[cypherTypeFlavor]);
       }
@@ -437,25 +496,16 @@ export class NumeneraPCActorSheet extends ActorSheet {
     });
 
     sheetData.displayCypherLimitWarning = this.actor.isOverCypherLimit();
+  }
 
-    sheetData.data.items.abilities = sheetData.data.items.abilities.map(ability => {
-      ability.nocost = (ability.data.cost.amount <= 0);
-      ability.ranges = NUMENERA.optionalRanges;
-      ability.stats = NUMENERA.stats;
-      ability.showIcon = ability.img && sheetData.settings.icons.abilities;
-      ability.data.notes = removeHtmlTags(ability.data.notes);
-      return ability;
-    });
-
-    sheetData.data.items.skills = sheetData.data.items.skills.map(skill => {
-      skill.stats = NUMENERA.stats;
-      skill.showIcon = skill.img && sheetData.settings.icons.skills;
-      skill.untrained = skill.data.skillLevel == 0;
-      skill.trained = skill.data.skillLevel == 1;
-      skill.specialized = skill.data.skillLevel == 2;
-      return skill;
-    });
-
+  /**
+   * Processes equipment-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setEquipmentData(sheetData) {
     sheetData.data.items.weapons = sheetData.data.items.weapons.map(weapon => {
       weapon.showIcon = weapon.img && sheetData.settings.icons.equipment;
       weapon.data.notes = removeHtmlTags(weapon.data.notes);
@@ -473,25 +523,42 @@ export class NumeneraPCActorSheet extends ActorSheet {
       equipment.data.notes = removeHtmlTags(equipment.data.notes);
       return equipment;
     });
+  }
 
-    if (sheetData.useOddities) {
-      sheetData.data.items.oddities = sheetData.data.items.oddities.map(oddity => {
-        oddity.editable = game.user.hasRole(game.settings.get("numenera", "cypherArtifactEdition"));
-        oddity.showIcon = oddity.img && sheetData.settings.icons.numenera;
-        oddity.data.notes = removeHtmlTags(oddity.data.notes);
-        return oddity;
-      });
-    }
+  /**
+   * Processes ability-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setAbilitiesData(sheetData) {
+    sheetData.data.items.abilities = sheetData.data.items.abilities.map(ability => {
+      ability.nocost = (ability.data.cost.amount <= 0);
+      ability.ranges = NUMENERA.optionalRanges;
+      ability.stats = NUMENERA.stats;
+      ability.showIcon = ability.img && sheetData.settings.icons.abilities;
+      ability.data.notes = removeHtmlTags(ability.data.notes);
+      return ability;
+    });
+  }
 
-    if (sheetData.usePowerShifts) {
-      sheetData.data.items.powerShifts = sheetData.data.items.powerShifts.map(powerShift => {
-        powerShift.showIcon = powerShift.img && sheetData.settings.icons.powerShifts;
-        powerShift.data.notes = removeHtmlTags(powerShift.data.notes);
-        return powerShift;
-      });
-    }
-
-    return sheetData;
+  /**
+   * Processes skill-related data for getData(). If you're calling this from another
+   * function, please consider reviewing your life's priorities.
+   *
+   * @param {*} sheetData
+   * @memberof NumeneraPCActorSheet
+   */
+  _setSkillsData(sheetData) {
+    sheetData.data.items.skills = sheetData.data.items.skills.map(skill => {
+      skill.stats = NUMENERA.stats;
+      skill.showIcon = skill.img && sheetData.settings.icons.skills;
+      skill.untrained = skill.data.skillLevel == 0;
+      skill.trained = skill.data.skillLevel == 1;
+      skill.specialized = skill.data.skillLevel == 2;
+      return skill;
+    });
   }
 
   /**
