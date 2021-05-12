5598b992adea2808b3f92c67c2e6bada1ce2588a
SolarBear <7608394+SolarBear@users.noreply.github.com>
2021-05-11 21:42:48 -0400

    Refactor of the structure, added support for createDocuments for 0.8

--- module/item/NumeneraItem.js
+++ module/item/NumeneraItem.js
@@ -10,6 +10,21 @@ import { NumeneraWeaponItem } from "./NumeneraWeaponItem.js";
 import { StrangeRecursionItem } from "./StrangeRecursionItem.js";
 import { NumeneraPowerShiftItem } from "./NumeneraPowerShiftItem.js";
 
+//Mapping of item type (string) to its related class
+const itemMappings = {
+  ability: NumeneraAbilityItem,
+  armor: NumeneraArmorItem,
+  artifact: NumeneraArtifactItem,
+  cypher: NumeneraCypherItem,
+  equipment: NumeneraEquipmentItem,
+  npcAttack: NumeneraNpcAttackItem,
+  oddity: NumeneraOddityItem,
+  powerShift: NumeneraPowerShiftItem,
+  recursion: StrangeRecursionItem,
+  skill: NumeneraSkillItem,
+  weapon: NumeneraWeaponItem,
+};
+
 /**
  * Numenera item base class
  *
@@ -27,91 +42,34 @@ export const NumeneraItem = new Proxy(function () {}, {
   //Calling a constructor from this proxy object
   construct: function (target, args) {
     const [data] = args;
-    switch (data.type) {
-      case "ability":
-        return new NumeneraAbilityItem(...args);
-      case "armor":
-        return new NumeneraArmorItem(...args);
-      case "artifact":
-        return new NumeneraArtifactItem(...args);
-      case "cypher":
-        return new NumeneraCypherItem(...args);
-      case "equipment":
-        return new NumeneraEquipmentItem(...args);
-      case "npcAttack":
-        return new NumeneraNpcAttackItem(...args);
-      case "oddity":
-        return new NumeneraOddityItem(...args);
-      case "powerShift":
-        return new NumeneraPowerShiftItem(...args);
-      case "skill":
-        return new NumeneraSkillItem(...args);
-      case "weapon":
-        return new NumeneraWeaponItem(...args);
-      case "recursion":
-        return new StrangeRecursionItem(...args);
-      default:
-        throw new Error("Unsupported Entity type for create(): " + data.type);
 
-    }
+    if (!itemMappings.hasOwnProperty(data.type))
+      throw new Error("Unsupported Entity type for create(): " + data.type);
+
+    return new itemMappings[data.type](...args);
   },
   //Property access on this weird, dirty proxy object
   get: function (target, prop, receiver) {  
     switch (prop) {
       case "create":
+      case "createDocuments":
         //Calling the class' create() static function
         return function (data, options) {
           if (data.constructor === Array) {
             //Array of data, this happens when creating Actors imported from a compendium
-            return data.map(a => NumeneraItem.create(a, options));
+            return data.map(i => NumeneraItem.create(i, options));
           }
 
-          switch (data.type) {
-            case "ability":
-              return NumeneraAbilityItem.create(data, options);
-            case "armor":
-              return NumeneraArmorItem.create(data, options);
-            case "artifact":
-              return NumeneraArtifactItem.create(data, options);
-            case "cypher":
-              return NumeneraCypherItem.create(data, options);
-            case "equipment":
-              return NumeneraEquipmentItem.create(data, options);
-            case "npcAttack":
-              return NumeneraNpcAttackItem.create(data, options);
-            case "oddity":
-              return NumeneraOddityItem.create(data, options);
-            case "powerShift":
-              return NumeneraPowerShiftItem.create(data, options);
-            case "skill":
-              return NumeneraSkillItem.create(data, options);
-            case "weapon":
-              return NumeneraWeaponItem.create(data, options);
-            case "recursion":
-              return StrangeRecursionItem.create(data, options);
-            default:
-              throw new Error(
-                "Unsupported Entity type for create(): " + data.type
-              );
-          }
+          if (!itemMappings.hasOwnProperty(data.type))
+            throw new Error("Unsupported Entity type for create(): " + data.type);
+
+          return itemMappings[data.type].create(data, options);
         };
-
+        
       case Symbol.hasInstance:
         //Applying the "instanceof" operator on the instance object
         return function (instance) {
-          return (
-            instance instanceof NumeneraAbilityItem ||
-            instance instanceof NumeneraArmorItem ||
-            instance instanceof NumeneraArtifactItem ||
-            instance instanceof NumeneraCypherItem ||
-            instance instanceof NumeneraEquipmentItem ||
-            instance instanceof NumeneraOddityItem ||
-            instance instanceof NumeneraNpcAttackItem ||
-            instance instanceof NumeneraPowerShiftItem ||
-            instance instanceof NumeneraSkillItem ||
-            instance instanceof NumeneraWeaponItem ||
-            instance instanceof StrangeRecursionItem
-          );
+          return Object.values(itemMappings).some(i => instance instanceof i);
         };
 
       default:
