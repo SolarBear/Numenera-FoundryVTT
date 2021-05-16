4ce591d1953d7be589b81631d89706453251535e
SolarBear <7608394+SolarBear@users.noreply.github.com>
2021-05-16 10:35:02 -0400

    Improved code to match NumeneraItem

--- module/actor/NumeneraActor.js
+++ module/actor/NumeneraActor.js
@@ -2,6 +2,14 @@ import { NumeneraCommunityActor } from "./NumeneraCommunityActor.js";
 import { NumeneraNPCActor } from "./NumeneraNPCActor.js";
 import { NumeneraPCActor } from "./NumeneraPCActor.js";
 
+//TODO NumeneraActor and NumeneraItem are almost identical, genericisize (???) them
+
+const actorMappings = {
+  pc: NumeneraPCActor,
+  npc: NumeneraNPCActor,
+  community: NumeneraCommunityActor,
+}
+
 /**
  * Numenera Actor base class
  *
@@ -18,58 +26,38 @@ export const NumeneraActor = new Proxy(function () {}, {
   //Calling a constructor from this proxy object
   construct: function (target, args) {
     const [data] = args;
-    switch (data.type) {
-      case "pc":
-        return new NumeneraPCActor(...args);
 
-      case "npc":
-        return new NumeneraNPCActor(...args);
+    if (!actorMappings.hasOwnProperty(data.type))
+      throw new Error("Unsupported Entity type for create(): " + data.type);
 
-      case "community":
-        return new NumeneraCommunityActor(...args);
-
-      default:
-        throw new Error("Unsupported Entity type for create(): " + data.type);
-    }
+    return new actorMappings[data.type](...args);
   },
   //Property access on this weird, dirty proxy object
-  get: function (target, prop, receiver) {
+  get: function (target, prop, receiver) {  
     switch (prop) {
       case "create":
+      case "createDocuments":
         //Calling the class' create() static function
         return function (data, options) {
           if (data.constructor === Array) {
             //Array of data, this happens when creating Actors imported from a compendium
-            return data.map(a => NumeneraActor.create(a, options));
+            return data.map(i => NumeneraActor.create(i, options));
           }
 
-          switch (data.type) {
-            
-            case "pc":
-              return NumeneraPCActor.create(data, options);
-            case "npc":
-              return NumeneraNPCActor.create(data, options);
-            case "community":
-              return NumeneraCommunityActor.create(data, options);
-            default:
-              throw new Error(
-                "Unsupported Entity type for create(): " + data.type
-              );
-          }
+          if (!actorMappings.hasOwnProperty(data.type))
+            throw new Error("Unsupported Entity type for create(): " + data.type);
+
+          return actorMappings[data.type].create(data, options);
         };
-
+        
       case Symbol.hasInstance:
         //Applying the "instanceof" operator on the instance object
         return function (instance) {
-          return (
-            instance instanceof NumeneraCommunityActor ||
-            instance instanceof NumeneraPCActor ||
-            instance instanceof NumeneraNPCActor
-          );
+          return Object.values(actorMappings).some(i => instance instanceof i);
         };
-        
+
       default:
-        //Just forward any requested properties to the base Actor class
+        //Just forward any requested properties to the base Item class
         return Actor[prop];
     }
   },
