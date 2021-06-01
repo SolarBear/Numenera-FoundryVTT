4f428695a3c59149d2014f190014b32830f6be50
SolarBear <7608394+SolarBear@users.noreply.github.com>
2021-05-31 21:41:15 -0400

    Fixed GM intrusions

--- numenera.js
+++ numenera.js
@@ -87,4 +87,4 @@ Hooks.once("ready", migrateWorld);
 Hooks.once("ready", numeneraSocketListeners);
 
 //Random hooks should go in there
-Hooks.once("ready", registerHooks);
+Hooks.once("init", registerHooks);
